52b6a9e90d8f5d0983b1a3d67e0dc266520fdaea
NickMarques005 <nicolas.marques005@gmail.com>
2024-01-10 18:31:57 -0300

    Update's Notification Controller

--- controller/notificationController.js
+++ controller/notificationController.js
@@ -10,7 +10,23 @@ exports.createNotification = async (notificationData) => {
     }
 
     try {
+        const decodedToken = jwt.verify(authToken, jwt_mainKey);
+        const userId = decodedToken.user.id;
 
+        const newNotification = new Notification({
+            user: userId,
+            title: notificationData.title,
+            body: notificationData.body,
+            data: notificationData.data,
+        });
+
+        const savedNotification = await newNotification.save();
+
+        if (!savedNotification) {
+            return res.status(400).json({ success: false, errors: ["Houve um erro ao salvar a notificação!"] });
+        }
+
+        return res.status(201).json({ success: true, notification: savedNotification });
 
     }
     catch (err) {
@@ -26,7 +42,11 @@ exports.getNotifications = async () => {
     }
 
     try {
+        const decodedToken = jwt.verify(authToken, jwt_mainKey);
+        const userId = decodedToken.user.id;
 
+        const notifications = await Notification.find({ user: userId }).sort({ createdAt: -1 });
+        return res.status(200).json({ success: true, notifications });
     }
     catch (err) {
         console.error("Erro ao criar notificação: ", err);
@@ -41,6 +61,20 @@ exports.deleteNotification = async () => {
     }
 
     try {
+        const decodedToken = jwt.verify(authToken, jwt_mainKey);
+        const userId = decodedToken.user.id;
+
+        const notificationId = req.body.notificationId;
+        const deletedNotification = await Notification.findOneAndDelete({
+            _id: notificationId,
+            user: userId,
+        });
+
+        if (!deletedNotification) {
+            return res.status(404).json({ success: false, errors: ["Notificação não encontrada"] });
+        }
+
+        return res.status(200).json({ success: true, message: "Notificação excluída com sucesso!" });
 
     }
     catch (err) {
@@ -56,7 +90,26 @@ exports.updateNotification = async () => {
     }
 
     try {
+        const decodedToken = jwt.verify(authToken, jwt_mainKey);
+        const userId = decodedToken.user.id;
 
+        const notificationId = req.body.notificationId;
+        const updatedData = req.body.notificationUpdate;
+
+        const updatedNotification = await Notification.findOneAndUpdate({
+            _id: notificationId,
+            user: userId,
+        },
+            { $set: updatedData },
+            { new: true }
+        );
+
+        if(!updatedNotification)
+        {
+            return res.status(404).json({ success: false, errors: ["Notificação não encontrada"] });
+        }
+
+        return res.status(200).json({ success: true, notification: updatedNotification, message: "Mensagem atualizada com sucesso!"});
     }
     catch (err) {
         console.error("Erro ao criar notificação: ", err);
