ea118d20b03b48c0b568a11d9055cafa59a520e7
NickMarques005 <nicolas.marques005@gmail.com>
2024-07-04 13:06:32 -0300

    Adjusting time zone conversion in Medication History creation

--- services/medications/medicationService.js
+++ services/medications/medicationService.js
@@ -1,7 +1,7 @@
 const Medication = require('../../models/medication');
 const { PatientMedicationHistory } = require('../../models/patient_history');
 const Treatment = require('../../models/treatment');
-const { getCurrentDateInBrazilTime } = require('../../utils/date/timeZones');
+const { getCurrentDateInBrazilTime, convertToBrazilTime } = require('../../utils/date/timeZones');
 
 const createNewMedicationHistory = async (medicationId, patientId, scheduleTime) => {
     try {
@@ -9,7 +9,7 @@ const createNewMedicationHistory = async (medicationId, patientId, scheduleTime)
         const treatment = await Treatment.findOne({patientId: patientId});
         if(!treatment) return console.error("Usuário não está em tratamento no momento");
         
-        const timeZoneScheduleTime = getCurrentDateInBrazilTime(scheduleTime);
+        const timeZoneScheduleTime = convertToBrazilTime(scheduleTime);
 
         const currentSchedule = timeZoneScheduleTime.toTimeString().slice(0, 5);
 
