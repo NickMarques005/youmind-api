58b0911268350e3a6dda95ed74183945f04013d4
NickMarques005 <nicolas.marques005@gmail.com>
2024-07-07 16:33:25 -0300

    Adjusted notification to handle sender avatar

--- database/streams/chat/operations/insertMessage.js
+++ database/streams/chat/operations/insertMessage.js
@@ -39,7 +39,7 @@ const handleInsertMessage = async (change) => {
                         name: senderMessage.name,
                         email: senderMessage.email,
                         id: senderId,
-                        avatar: senderIcon
+                        avatar: senderMessage.avatar || ''
                     },
                     show_modal: false,
                     redirect_params: {
